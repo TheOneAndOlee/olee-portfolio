ef87bba645727b0fb51203a7d9fa544e895cfb8d
Osmond Lee <olee081206@gmail.com>
2025-08-15 21:42:16 -0500

    Changed page structure again, now has separate sections for future topics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,52 +21,53 @@
 
 <!-- <Navbar /> -->
 
-<div class="relative min-h-screen w-full bg-[#070809] text-[#FCFFF9]"
+<div class="relative w-full bg-[#070809] text-[#FCFFF9]"
     style="font-family: 'geo-regular', sans-serif;">
     <Sparkles
         minSize={0.8}
         maxSize={2}
         speed={3}
-        particleDensity={200}
+        particleDensity={300}
         className="absolute inset-0 w-full h-full pointer-events-none"
     />
-
-    <div class="py-8">
-        <Typewriter 
-            mode="cascade" 
-            interval={50} 
-            keepCursorOnFinish={false} 
-            cursor={false}
-            on:done={() => showNext = true}
-        >
-            <p class="text-8xl justify-center text-center flex items-center w-full text-[#FCFFF9]">Hey! I'm</p>
-        </Typewriter>
-
-        {#if showNext}
+    <div class="h-screen flex flex-col">
+        <div class="py-8">
             <Typewriter 
                 mode="cascade" 
-                interval={75} 
-                keepCursorOnFinish={false}
+                interval={50} 
+                keepCursorOnFinish={false} 
                 cursor={false}
-                on:done={() => showIntro = true} 
+                on:done={() => showNext = true}
             >
-                <p class="text-9xl justify-center text-center flex items-center w-full text-[#F7B71F]">Osmond Lee!</p>
+                <p class="text-8xl justify-center text-center flex items-center w-full text-[#FCFFF9]">Hey! I'm</p>
             </Typewriter>
-        {/if}
-    </div>
 
-    <div class="py-8">
-        <div class="px-12 m-2">
-            <div class="" id="Intro">
-                <p class="text-4xl flex w-full text-[#FCFFF9]">
+            {#if showNext}
+                <Typewriter 
+                    mode="cascade" 
+                    interval={75} 
+                    keepCursorOnFinish={false}
+                    cursor={false}
+                    on:done={() => showIntro = true} 
+                >
+                    <p class="text-9xl justify-center text-center flex items-center w-full text-[#F7B71F]">Osmond Lee!</p>
+                </Typewriter>
+            {/if}
+        </div>
+
+        <div class="flex-grow flex w-full text-[#FCFFF9] px-12 m-2" id="Intro">
+            <div>
+                <p class="text-4xl text-[#FCFFF9]">
                     I'm a student programmer who loves games, making them, and all things cool! 
                     I'm still pretty new to applied programming, but I'm learning more every day! <br> <br>
                 </p>
-                <p class="text-4xl py-8 flex w-full text-[#FCFFF9]">
+                <p class="text-4xl text-[#FCFFF9]">
                     If you want to know more about me, check out my blog over <a href="#about" class="hover:underline inline">here</a>.
                 </p>
             </div>
         </div>
-
+    </div>
+    <div class="h-screen py-8 px-12 m-2 w-full text-[#FCFFF9]" id="About">
+        <p class="text-8xl py-8 justify-center items-center text-center">About Me</p>
     </div>
 </div>
