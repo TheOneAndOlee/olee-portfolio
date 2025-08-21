d5f0b9344ef69b0a29ec13eb80d33ea12f543fca
Osmond Lee <olee081206@gmail.com>
2025-08-21 00:12:14 -0500

    Added first part of contact page, added the 3D pin effect

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -37,36 +37,36 @@
             </Typewriter>
         </div>
 
-            <div class="flex-grow grid grid-cols-5 w-full text-[#FCFFF9] px-12 m-2" 
-                style="font-family: 'atkinson-hyperlegible-next', sans-serif;"
-            >
-                {#if showImage}
-                <div class="col-span-1" transition:fly={{delay:250, duration:300, x:-100, y:0}} on:introend={() => showText = true}>
-                    <enhanced:img 
-                        class="relative z-50 w-full h-auto object-cover rounded-xl" 
-                        src="/src/lib/assets/betterHeadshot.webp" 
-                        sizes="(min-width: 640px) 768px, (min-width: 1920px) 1280px, (min-width-1080) 640px" 
-                        alt="An alright pic of me!" 
-                    />
+        <div class="flex-grow grid grid-cols-5 w-full text-[#FCFFF9] px-12 m-2" 
+            style="font-family: 'atkinson-hyperlegible-next', sans-serif;"
+        >
+            {#if showImage}
+            <div class="col-span-1" transition:fly={{delay:250, duration:300, x:-100, y:0}} on:introend={() => showText = true}>
+                <enhanced:img 
+                    class="relative z-50 w-full h-auto object-cover rounded-xl" 
+                    src="/src/lib/assets/betterHeadshot.webp" 
+                    sizes="(min-width: 640px) 768px, (min-width: 1920px) 1280px, (min-width-1080) 640px" 
+                    alt="An alright pic of me!" 
+                />
+            </div>
+            {/if}
+
+            <div class="col-span-4 px-8">
+                {#if showText} 
+                <div transition:fly={{delay:0, duration:300, x:-100, y:0}}>
+                    <p class="text-3xl text-[#FCFFF9]">
+                        I'm Osmond Lee, a Computer Science major with a Game Studies and Design minor at the University of Illinois Urbana Champaign (UIUC).
+                    </p>
+                    <p class="text-3xl py-8 text-[#FCFFF9]">
+                        I've grown up playing games my whole life, and I've come to appreciate what I call "invisible design" in the media I consume.
+                    </p>
+                    <p class="text-3xl text-[#FCFFF9]">
+                        In no small part due to my history with and love for games, my dream is to make my own games/media that combine my passion for programming with my love for invisible design.
+                    </p>
                 </div>
                 {/if}
-
-                <div class="col-span-4 px-8">
-                    {#if showText} 
-                    <div transition:fly={{delay:0, duration:300, x:-100, y:0}}>
-                        <p class="text-3xl text-[#FCFFF9]">
-                            I'm Osmond Lee, a Computer Science major with a Game Studies and Design minor at the University of Illinois Urbana Champaign (UIUC).
-                        </p>
-                        <p class="text-3xl py-8 text-[#FCFFF9]">
-                            I've grown up playing games my whole life, and I've come to appreciate what I call "invisible design" in the media I consume.
-                        </p>
-                        <p class="text-3xl text-[#FCFFF9]">
-                            In no small part due to my history with and love for games, my dream is to make my own games/media that combine my passion for programming with my love for invisible design.
-                        </p>
-                    </div>
-                    {/if}
-                </div>
             </div>
+        </div>
 
 
     </div>
--- src/routes/Contact/+page.svelte
+++ src/routes/Contact/+page.svelte
@@ -4,9 +4,7 @@
     import Typewriter from 'svelte-typewriter';
 	import { fly } from 'svelte/transition';
 
-    let showNext = false;
-    let showIntro = false;
-    let showButtons = false;
+    let showLinks = false;
 </script>
 
 <!-- For Credits:
@@ -25,48 +23,29 @@
         particleDensity={300}
         className="absolute inset-0 w-full h-full pointer-events-none"
     />
-    <div class="h-screen flex flex-col">
-        <div class="py-8">
+
+        <div class="py-8"
+            style="font-family: 'geo-regular', sans-serif;"
+        >
             <Typewriter 
                 mode="cascade" 
-                interval={50} 
+                interval={25} 
                 keepCursorOnFinish={false} 
                 cursor={false}
-                on:done={() => showNext = true}
+                on:done={() => showLinks = true}
             >
-                <p class="text-8xl justify-center text-center flex items-center w-full text-[#FCFFF9]">Hey! I'm</p>
+                <p class="text-8xl justify-center text-center flex items-center w-full text-[#FCFFF9]">Contact Me!</p>
             </Typewriter>
-
-            {#if showNext}
-                <Typewriter 
-                    mode="cascade" 
-                    interval={75} 
-                    keepCursorOnFinish={false}
-                    cursor={false}
-                    on:done={() => showIntro = true} 
-                >
-                    <p class="text-9xl justify-center text-center flex items-center w-full text-[#F7B71F]">Osmond Lee!</p>
-                </Typewriter>
-            {/if}
         </div>
 
-        {#if showIntro}
-            <div class="flex-grow flex w-full text-[#FCFFF9] px-12 m-2" id="Intro" transition:fly={{delay:100, duration:300, x:0, y:150}} on:introend={() => showButtons = true}>
-                <div>
-                    <p class="text-4xl text-[#FCFFF9]">
-                        I'm a student programmer who loves games, making them, and all things cool! 
-                        I'm still pretty new to applied programming, but I'm learning more every day! <br> <br>
-                    </p>
-                </div>
-            </div>
-            {#if showButtons}
-                <div></div>
-            {/if}
-        {/if}
+        <div class=""
+            style="font-family: 'atkinson-hyperlegible-next', sans-serif;"
+        >
+            
+        </div>
 
-
-    </div>
-    <div class="h-screen py-8 px-12 m-2 w-full text-[#FCFFF9]" id="About">
-        <p class="text-8xl py-8 justify-center items-center text-center">About Me</p>
-    </div>
+        <!-- TODO 
+            1. Find carousel effect, or a list of buttons for different contacts
+            2. Contacts to add: LinkedIn, Github, and Email 
+        -->
 </div>
